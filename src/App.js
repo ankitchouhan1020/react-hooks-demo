279fef00f97983302bf1606913ee31617374ce1a
Ankit Chouhan <ankitchouhan1020@gmail.com>
2022-04-14 00:17:19 +0530

    feat: initial project setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import "./styles.css";
+import React, { useState } from 'react';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => setCount(count => count + 1);
+  const decrementCount = () => setCount(count => count - 1);
+  const resetCount = () => setCount(0);
+
+  const countColor = count > 0 ? '#FFEA00' : count < 0 ? '#231f20' : '#fff';
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div class="counter">
+        <button
+          class="increase arrow btn"
+          onClick={incrementCount}
+        >
+          <i class="fas fa-chevron-up"></i>
+        </button>
+
+        <span class="value" style={{ color: countColor }}>{count}</span>
+
+        <button
+          class="decrease arrow btn"
+          onClick={decrementCount}
+        >
+          <i class="fas fa-chevron-down"></i>
+        </button>
+      </div>
+
+      <button class="reset btn" onClick={resetCount}>Reset</button>
+    </>
   );
 }
